0cc307b0d11078e71242e362fb614adf758512fb
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-05-31 23:55:35 +0100

    copyright

--- usb.h
+++ usb.h
@@ -1,3 +1,23 @@
+/* Copyright (c) 2013 Richard Russon (FlatCap)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _USB_H_
+#define _USB_H_
+
 #include "usbtypes.h"
 
 /**
@@ -80,4 +100,6 @@ typedef struct _command_status_wrapper {
 	u8   bCSWStatus;	// 0x0C
 } __attribute__((__packed__)) command_status_wrapper;
 
+
+#endif // _USB_H_
 
